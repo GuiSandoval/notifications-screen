a53a73f60b0dfdb2a72046442dea16a4c5e6cef4
Guilherme Sandoval <guijpjs@gmail.com>
2023-04-18 21:28:49 -0300

    Create Structure to Page Notifications

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles';
+
+function Header() {
+  return (
+    <S.Container>Header</S.Container>
+  );
+}
+
+export { Header };
--- src/pages/Notifications/index.tsx
+++ src/pages/Notifications/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { apiUrl } from '../../services/apiUrl';
+import * as S from './styles';
+import { Header } from '../../components/Header';
 
 function NotificationsPage() {
+  const [notifications, setNotifications] = useState([] as any[]);
+
+  async function loadNotifications() {
+    const { data: notificationsresponse } = await apiUrl.get('/posts?userId=1');
+    setNotifications(notificationsresponse);
+  }
+
+  useEffect(() => {
+    loadNotifications();
+  }, []);
+
+  if (!notifications) return <div>Loading...</div>;
+
   return (
-    <div>NotificationsPage</div>
+    <S.Container>
+      <Header />
+    </S.Container>
   );
 }
 
